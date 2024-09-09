1fe78299e85e469d6877f5aa4cad508d352e5cec
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-09-09 13:32:02 +0700

    fix: change api path axios

--- app/service/axios.js
+++ app/service/axios.js
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://10.10.102.115:8080/api", // IP Mirza
+  baseURL: "http://10.10.102.115:8080/api/v1", // IP Mirza
   // baseURL: "http://10.10.102.137:8080/api", // IP PM Iklil
   // baseURL: "http://localhost:8080/api", // My IP
 });
